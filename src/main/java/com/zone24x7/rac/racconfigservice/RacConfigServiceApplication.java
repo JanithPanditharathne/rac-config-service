ec668a95237047e75535741fbf44205e7011a3fa
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-04-20 10:26:02 +0530

    Initial commit.

---
+++ src/main/java/com/zone24x7/rac/racconfigservice/RacConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zone24x7.rac.racconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zone24x7/rac/racconfigservice/RacConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zone24x7.rac.racconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
