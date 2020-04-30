9fdfaa116baa12a5d0a6c2fdae10a2b5819a8d31
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-04-30 10:13:35 +0530

    Added main controllers

---
+++ src/main/java/com/zone24x7/rac/configservice/rec/RecController.java
@@ -0,0 +1,9 @@
+package com.zone24x7.rac.configservice.rec;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class RecController {
+}
