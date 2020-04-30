4a68db04b91dc27668add5dffc01d3c30e901376
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-04-30 10:07:29 +0530

    Updated db creation script with rec engine table creation.

--- src/main/java/com/zone24x7/rac/configservice/dbscripts/creation_script.sql
+++ src/main/java/com/zone24x7/rac/configservice/dbscripts/creation_script.sql
@@ -2,10 +2,18 @@
 -- use database --
 use ibraccs;
 
+-- rec engine table --
+CREATE TABLE `rec_engine` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `config_type` varchar(45) DEFAULT NULL,
+  `config_json` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
 -- create algorithm table --
 CREATE TABLE `algorithm` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `description` longtext DEFAULT NULL,
   `default_display_text` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
