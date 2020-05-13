50a2923cc691c1b06856697d03cb9d41ac720d3e
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-05-13 16:46:01 +0530

    Enable cross domain access for api calls.

--- src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
+++ src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
@@ -4,9 +4,24 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RacConfigServiceApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/v1/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 
     @Bean
     public ModelMapper getModelMapper() {
