22ac33a6f896bc22bd6d44832ba47d7d016019db
MariamZ <mariamz@zone24x7.com>
2020-05-14 11:31:53 +0530

    Introduced the initial structure for bundles.

---
+++ src/main/java/com/zone24x7/rac/configservice/bundle/Bundle.java
@@ -0,0 +1,61 @@
+package com.zone24x7.rac.configservice.bundle;
+
+import javax.persistence.*;
+
+@Entity
+public class Bundle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @Column(name = "default_limit")
+    private int defaultLimit;
+
+    @Column(name = "combine_enabled")
+    private boolean combineEnabled;
+
+    @Column(name = "combine_display_text")
+    private String combineDisplayText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDefaultLimit() {
+        return defaultLimit;
+    }
+
+    public void setDefaultLimit(int defaultLimit) {
+        this.defaultLimit = defaultLimit;
+    }
+
+    public boolean isCombineEnabled() {
+        return combineEnabled;
+    }
+
+    public void setCombineEnabled(boolean combineEnabled) {
+        this.combineEnabled = combineEnabled;
+    }
+
+    public String getCombineDisplayText() {
+        return combineDisplayText;
+    }
+
+    public void setCombineDisplayText(String combineDisplayText) {
+        this.combineDisplayText = combineDisplayText;
+    }
+}
---
+++ src/main/java/com/zone24x7/rac/configservice/bundle/BundleRepository.java
@@ -0,0 +1,10 @@
+package com.zone24x7.rac.configservice.bundle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for channel.
+ *
+ */
+public interface BundleRepository extends JpaRepository<Bundle, Integer> {
+}
