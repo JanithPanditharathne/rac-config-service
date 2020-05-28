e469308018f1c7991879ffc3c36736477c11a227
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-05-28 13:25:24 +0530

    Revert "Fix sonar issues."
    This reverts commit cc4089a9

--- src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
+++ src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
@@ -29,7 +29,7 @@ public class RacConfigServiceApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(RacConfigServiceApplication.class);
+        SpringApplication.run(RacConfigServiceApplication.class, args);
     }
 
 }
