b492160eefed4eb3e55a6ce71114e3437c9617d0
Dinesh Widanapathirana <dineshw@zone24x7.com>
2020-06-09 14:11:59 +0530

    Minor update

--- src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
+++ src/main/java/com/zone24x7/rac/configservice/RacConfigServiceApplication.java
@@ -28,7 +28,7 @@ public class RacConfigServiceApplication {
         return new ModelMapper();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // NOSONAR
         SpringApplication.run(RacConfigServiceApplication.class, args); // NOSONAR
     }
 
